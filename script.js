c839ece9f1db2a8701bc6b993c3f238ce696c16f
Janaina P de Oliveira Minutti <janainapp@gmail.com>
2021-03-09 21:50:59 -0300

    ultima tentativa de correção de hj req 4

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function load() {
   let action = document.getElementById('pixel-board').style.background = 'white';
-  let blacColor = document.getElementsByClassName('selected').style.background = 'black';
 }
 window.onload = load;
