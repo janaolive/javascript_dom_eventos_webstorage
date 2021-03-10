2051930e337c377743c43383eed8ef52ed84bd83
Janaina P de Oliveira Minutti <janainapp@gmail.com>
2021-03-09 21:43:18 -0300

    correcao requisito 4

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function load() {
-  let action = document.getElementById('color1').style.background = 'black';
+  let action = document.getElementById('pixel-board').style.background = 'white';
+  let blacColor = document.getElementsByClassName('selected').style.background = 'black';
 }
 window.onload = load;
