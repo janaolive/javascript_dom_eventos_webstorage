3d3ec5b0dcd69062041f9088e1e95454aff21441
Janaina P de Oliveira Minutti <janainapp@gmail.com>
2021-03-10 20:01:29 -0300

    requisito 8

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ for (let index = 0; index < palleteColors.length; index += 1) {
     event.target.classList.add('selected');
   });
 }
+
+let colorPixels = document.querySelectorAll('.pixel');
+
+for(let indexPixel = 0; indexPixel < colorPixels.length; indexPixel += 1){
+  colorPixels[indexPixel].addEventListener('click', function(){
+    let newColor = document.getElementsByClassName('selected')[0].style.backgroundColor;
+    colorPixels[indexPixel].style.backgroundColor = newColor;
+  })
+}
