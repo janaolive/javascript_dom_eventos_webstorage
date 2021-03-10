4a9cf6506917c730fda8ed0bfeec7edfa68d4754
Janaina P de Oliveira Minutti <janainapp@gmail.com>
2021-03-10 17:16:42 -0300

    requisito

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-/* function loadPixelArt() {
-  let element = document.getElementsByClassName('color') [0];
-  element.className += 'color';
-}
-window.onload = loadPixelArt(); */
+let palleteColors = document.querySelectorAll('.color');
 
+for (let index = 0; index < palleteColors.length; index += 1) {
+  palleteColors[index].addEventListener('click', function () {
+    let selectedClass = document.getElementsByClassName('selected')[0];
+    selectedClass.classList.remove('selected');
+    event.target.classList.add('selected');
+  });
+}
