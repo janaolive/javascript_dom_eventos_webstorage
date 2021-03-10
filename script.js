98c83ccb74adf9bb931b6c24c6ae788c1cb853b5
Janaina P de Oliveira Minutti <janainapp@gmail.com>
2021-03-10 15:16:40 -0300

    correção requisito 5

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function loadPixelArt() {
-  let element = document.querySelector('color1');
-  element.className += ' selected';
+  let element = document.getElementsByClassName('color') [0];
+  element.className += 'selected';
 }
 window.onload = loadPixelArt();
-function selected() {
-}
+
